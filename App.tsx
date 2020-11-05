9bcefb3ba300ecffe6a08fc132b8fac94196c490
theMistletoe <katachiwotodomete@gmail.com>
2020-11-05 10:36:14 +0900

    Modalとりあえず実装

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, FlatList, TextInput, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TextInput, Button, Alert, Modal, TouchableHighlight } from 'react-native';
 
 type TaskItem = {
   title: string;
@@ -11,6 +11,8 @@ export default function App() {
 
   const [tasks, setTasks] = useState<TaskItem[]>([]);
   const [inputText, setInputText] = useState<string>('');
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const [selectedIndex, setSelectedIndex] = useState<number>(0);
 
   useEffect(() => {
     setTasks([
@@ -26,8 +28,37 @@ export default function App() {
     setTasks(newNameList);
   };
 
+  const handlePressTaskItem = (index: number) => {
+    console.log("aaaaaaaaaaaaaaaa", index);
+    setSelectedIndex(index);
+    setModalVisible(true);
+  };
+
   return (
     <View style={styles.container}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+
+            <TouchableHighlight
+              style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+              onPress={() => {
+                setModalVisible(!modalVisible);
+              }}
+            >
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
       <TextInput
@@ -42,8 +73,16 @@ export default function App() {
       />
       <FlatList
         data={tasks}
-        renderItem={({item}) => <Text style={styles.item}>{item.title}</Text>}
+        renderItem={({item, index}) => <Text style={styles.item} key={index} onPress={() => handlePressTaskItem(index)}>{item.title}</Text>}
       />
+      <TouchableHighlight
+        style={styles.openButton}
+        onPress={() => {
+          setModalVisible(true);
+        }}
+      >
+        <Text style={styles.textStyle}>Show Modal</Text>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -60,4 +99,40 @@ const styles = StyleSheet.create({
     fontSize: 18,
     height: 44,
   },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  },
+  openButton: {
+    backgroundColor: "#F194FF",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
 });
