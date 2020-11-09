eef0825d8f25c963d680ee6dc208709e80c2ef32
theMistletoe <katachiwotodomete@gmail.com>
2020-11-09 17:55:48 +0900

    詳細画面からホーム画面に戻る実装

--- pages/Detail.tsx
+++ pages/Detail.tsx
@@ -1,24 +1,27 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import TaskDescription from '../components/TaskDescription';
 import { TaskItem } from '../types/TaskItem';
 
-type Props = {
-  task: TaskItem;
-}
-
 // TODO ここのrouteの型推論はv5で聞かせられるようになってるらしい
 // https://qiita.com/shinnoki/items/e32e20b812606ce7219c
 export default function Detail({ route }) {
+  const navigation = useNavigation(); // (2)
+
   const { itemTitle, itemDescription } = route.params;
   const task:TaskItem = {
     title: itemTitle,
     description: itemDescription,
   };
 
+  const handlePressOnCloseButton = () => {
+    navigation.goBack()
+  };
+
   return (
     <View style={styles.container}>
-      <TaskDescription task={task} />
+      <TaskDescription task={task} handlePress={handlePressOnCloseButton} />
     </View>
   );
 }
