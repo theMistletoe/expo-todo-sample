88b02133942a982e19c0f483561d739b0a42d05b
theMistletoe <katachiwotodomete@gmail.com>
2020-11-09 16:42:31 +0900

    とりあえず画面遷移の実装サンプルはできた

--- App.tsx
+++ App.tsx
@@ -5,6 +5,46 @@ import { TaskItem } from './types/TaskItem';
 import TaskList from './components/TaskList';
 import TaskDescription from './components/TaskDescription';
 
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
+
+function HomeScreen() {
+  const navigation = useNavigation(); // (2)
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <Text>Home Screen</Text>
+      <Button
+        title="詳細へ"
+        onPress={() => navigation.navigate('Detail')} />{/* (3) */}
+    </View>
+  );
+}
+
+function DetailScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <Text>Detail Screen</Text>
+    </View>
+  );
+}
+
+// (1) StackNavigatorを生成する
+const Stack = createStackNavigator();
+
+
 export default function App() {
 
   const [tasks, setTasks] = useState<TaskItem[]>([]);
@@ -36,33 +76,45 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert("Modal has been closed.");
-        }}
-      >
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <TaskDescription task={tasks[selectedIndex]} handlePress={handlePressModalCloseButton} />
-          </View>
-        </View>
-      </Modal>
-      <TextInput
-        style={{height: 40}}
-        placeholder="Type here to translate!"
-        onChangeText={text => setInputText(text)}
-        value={inputText}
-      />
-      <Button
-        title="Press me"
-        onPress={handlePress}
-      />
-      <TaskList tasks={tasks} handlePressTaskItem={handlePressTaskItem} />
-    </View>
+    // <View style={styles.container}>
+    //   <Modal
+    //     animationType="slide"
+    //     transparent={true}
+    //     visible={modalVisible}
+    //     onRequestClose={() => {
+    //       Alert.alert("Modal has been closed.");
+    //     }}
+    //   >
+    //     <View style={styles.centeredView}>
+    //       <View style={styles.modalView}>
+    //         <TaskDescription task={tasks[selectedIndex]} handlePress={handlePressModalCloseButton} />
+    //       </View>
+    //     </View>
+    //   </Modal>
+    //   <TextInput
+    //     style={{height: 40}}
+    //     placeholder="Type here to translate!"
+    //     onChangeText={text => setInputText(text)}
+    //     value={inputText}
+    //   />
+    //   <Button
+    //     title="Press me"
+    //     onPress={handlePress}
+    //   />
+    //   <TaskList tasks={tasks} handlePressTaskItem={handlePressTaskItem} />
+    // </View>
+    <NavigationContainer>
+      {/* (1) 画面遷移対象のアプリケーションコード */}
+      <Stack.Navigator initialRouteName="Home">
+        {/* (2) StackNavigator用の画面を定義する */}
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen} />
+        <Stack.Screen
+          name="Detail"
+          component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
