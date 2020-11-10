f34516eea1b1052b2ef240d66075007303535ad5
theMistletoe <katachiwotodomete@gmail.com>
2020-11-10 11:11:59 +0900

    Redux dev toolsの利用のための記述で型情報で怒られるのをきちんと買いた

--- redux/store.ts
+++ redux/store.ts
@@ -1,4 +1,15 @@
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import rootReducer from "./rootReducer";
 
-export const store = createStore(rootReducer);
+interface ExtendedWindow extends Window {
+  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+};
+declare var window: ExtendedWindow;
+
+const composeReduxDevToolsEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middlewares:any = [];
+
+export const store = createStore(
+  rootReducer,
+  composeReduxDevToolsEnhancers(applyMiddleware(...middlewares))
+);
