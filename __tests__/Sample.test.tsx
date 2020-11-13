bb3dcf4bbb588865105c52416ffd5d235517ab17
theMistletoe <katachiwotodomete@gmail.com>
2020-11-13 17:11:44 +0900

    インタラクティブなUnitTestの実装

--- __tests__/Sample.test.tsx
+++ __tests__/Sample.test.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { fireEvent, render } from '@testing-library/react-native';
 import renderer from 'react-test-renderer';
 import Sample from '../components/Sample';
 
-describe("Snapshot test sample", () => {
+describe("test Sample", () => {
   it('renders correctly with defaults', () => {
-    const comp = renderer.create(<Sample number={10} />)
+    const comp = renderer.create(<Sample text="XxXxX" />)
     expect(comp).toMatchSnapshot()
+  })
+
+  it('increments number by pressed button', () => {
+    const { getByText } = render(<Sample text="XxXxX" />);
+    fireEvent.press(getByText("increment"));
+    fireEvent.press(getByText("increment"));
+    expect(getByText("2")).toBeTruthy();
   })
 })
--- components/Sample.tsx
+++ components/Sample.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Text, View } from 'react-native';
 
 type Props = {
-  number: number
+  text: string
 }
 
 const Sample = (props: Props) => {
+  const [number, setNumber] = useState<number>(0);
+
+  const handlePress = () => {
+    const updateNumber = number + 1;
+    setNumber(updateNumber);
+  };
 
   return (
-    <Text>
-      {props.number}!!!
-    </Text>
+    <View>
+      <Text>
+        {props.text}!!!
+      </Text>
+      <Text>{number}</Text>
+      <Button onPress={handlePress} title="increment" />
+    </View>
   );
 };
 
